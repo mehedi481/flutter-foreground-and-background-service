c2ca4ff7dd7ed031849a097a0921293528e2ab1c
Mehedi <mehedihasanmax95@gmail.com>
2022-06-07 11:53:19 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/background_foreground_service_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.background_foreground_service_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Foreground and Background service",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Foreground and Background service")),
+      body: const Center(
+        child: Text("Foreground and Background service"),
+      ),
+    );
+  }
+}
